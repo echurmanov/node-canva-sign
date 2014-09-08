64bc036e14ff054910271da65cf6b4cea94ec9e5
echurmanov <eugene@soft-rose.com>
2014-09-08 11:56:45 +0600

    Создана демо версия рисорвалки с сохранением

---
+++ app.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var images = [];
+
+app.get('/', function(req, res){
+    res.sendFile('index.html', {"root": "./"});
+});
+app.get('/image', function(req, res){
+    var imageIndex = req.query.i;
+    if (typeof images[imageIndex] !== 'undefined') {
+        res.setHeader("Content-Type", "image/png");
+        res.send(images[imageIndex]);
+    } else {
+       res.send("");
+    }
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+    socket.on('save image', function (imageBase64){
+        console.log(imageBase64);
+        var imageData = new Buffer(imageBase64.substr(imageBase64.indexOf(',') + 1), 'base64');
+        images.push(imageData);
+        socket.emit("show image", images.length - 1)
+    });
+});
